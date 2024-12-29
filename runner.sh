d9bc16fc91428fe01ff825c3bee8602abff0b5d1
movixdestiny <movixdestiny@gmail.com>
2024-12-29 23:04:58 +0100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -5,18 +5,28 @@ CCMINER_DIR="ccminer"
 CCMINER_FILE="$CCMINER_DIR/ccminer"
 CONFIG_FILE="$CCMINER_DIR/config.json"
 
-# Check if ccminer and config.json are available
-if [[ -f "$CCMINER_FILE" && -f "$CONFIG_FILE" ]]; then
-    echo "ccminer and config.json found. Running..."
-    chmod +x "$CCMINER_FILE"
-    ./"$CCMINER_FILE" -c "$CONFIG_FILE"
-else
-    echo "ccminer or config.json not found. Setting up..."
+# Function to set up ccminer
+setup_ccminer() {
+    echo "Setting up ccminer and config.json..."
     yes | pkg update && yes | pkg upgrade &&
     yes | pkg install libjansson wget nano &&
     mkdir -p "$CCMINER_DIR" && cd "$CCMINER_DIR" &&
     wget -q --show-progress https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer &&
     wget -q --show-progress https://raw.githubusercontent.com/movixdestiny/pre-compiled/refs/heads/generic/config.json &&
-    chmod +x "$CCMINER_FILE" &&
+    chmod +x "$CCMINER_FILE"
+}
+
+# Check if ccminer and config.json are available
+if [[ -f "$CCMINER_FILE" && -f "$CONFIG_FILE" ]]; then
+    echo "ccminer and config.json found. Running..."
+else
+    setup_ccminer
+fi
+
+# Run ccminer
+if [[ -f "$CCMINER_FILE" && -f "$CONFIG_FILE" ]]; then
     ./"$CCMINER_FILE" -c "$CONFIG_FILE"
+else
+    echo "Setup failed. ccminer or config.json is missing."
+    exit 1
 fi
